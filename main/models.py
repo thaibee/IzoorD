cd699b80c2f4f879cba2f84a7f9a186c2d042a27
magadanthai <doctor.madbee@gmail.com>
2022-02-13 18:19:18 +0700

    Mssql UUID can use as well

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from IzoorD.mssql_uuid import MssqlUUID
 
-# Create your models here.
+
+class Testing(models.Model):
+    id = MssqlUUID(primary_key=True, max_length=36, editable=False, default=None)
+    name = models.CharField(max_length=20)
+    amount = models.DecimalField(max_digits=10, decimal_places=4)
+
+    class Meta:
+        db_table = 'testing'
+
+    def __str__(self):
+        return self.name
